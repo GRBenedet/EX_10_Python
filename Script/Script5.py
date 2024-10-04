0927d5d06709d1927a0cad4535367de4888eb6e6
Giovanni Benedet Rodrigues <gi-benedet@hotmail.com>
2024-10-04 10:10:16 -0300

    EX_10_parte_5
    Melhorando o código

--- Script/Script5.py
+++ Script/Script5.py
@@ -1,10 +1,31 @@
 #Refaça o DESAFIO 51, lendo o primeiro termo e a razão de uma PA, mostrando os 10 primeiros termos da progressão usando a estrutura while.
 
-num = int(input('defina um valor: '))
-r = int(input('defina a razão: '))
-c = 0
+a = 1
 
-while c != 11:
-    print('{} | '.format(num), end='')
-    num += r
-    c += 1
+while a != 0:
+
+    num = int(input('defina um valor: '))
+    r = int(input('defina a razão: '))
+    t = int(input('Defina a quantidade de termos: ')) + 1
+    c = 0
+
+    while c !=  t:
+        print('{} | '.format(num), end='')
+        num += r
+        c += 1
+
+    p = 1
+    
+    while p != 0:
+
+        op = str(input('quer fazer outra razão [S/N]? ')).upper() [0]
+
+        if op == 'S':
+            p = 0
+
+        elif op == 'N':
+            p = a = 0
+            print('fim do programa...')
+
+        else: 
+            print('opção invalida!')
